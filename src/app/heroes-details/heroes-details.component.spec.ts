7add81f8360c47bdb8911c2e5e67d6b16d5efef9
Ezequiel Mansilla <ezequielmansilla@outlook.com>
2021-05-25 14:49:04 -0300

    Some test added

--- src/app/heroes-details/heroes-details.component.spec.ts
+++ src/app/heroes-details/heroes-details.component.spec.ts
@@ -1,23 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
-import { HeroesApiService } from '../sdk-heroes';
+import { HeroDetails, HeroesApiService } from '../sdk-heroes';
 import { DialogService, ToastService } from '../shared/services';
 import { HeroesDetailsComponent } from './heroes-details.component';
+
+const heroMock: HeroDetails = {
+  id: 1,
+  name: 'Test',
+};
 
 describe('HeroesDetailsComponent', () => {
   let component: HeroesDetailsComponent;
   let fixture: ComponentFixture<HeroesDetailsComponent>;
+  let heroesApiService: jasmine.SpyObj<HeroesApiService>;
 
   beforeEach(async(() => {
+    heroesApiService = jasmine.createSpyObj<HeroesApiService>([
+      'getHeroDetails',
+      'updateHero',
+      'addHero',
+    ]);
+
     TestBed.configureTestingModule({
       imports: [RouterTestingModule, ReactiveFormsModule],
       declarations: [HeroesDetailsComponent],
       providers: [
         {
           provide: HeroesApiService,
-          useValue: {},
+          useValue: heroesApiService,
         },
         {
           provide: ToastService,
@@ -34,10 +47,12 @@ describe('HeroesDetailsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeroesDetailsComponent);
     component = fixture.componentInstance;
+    heroesApiService.getHeroDetails.and.returnValue(of(heroMock));
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
--- src/app/heroes-list/heroes-list.component.spec.ts
+++ src/app/heroes-list/heroes-list.component.spec.ts
@@ -2,41 +2,54 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
 
-import { HeroesApiService } from '../sdk-heroes';
+import { HeroDetails, HeroesApiService } from '../sdk-heroes';
 import { DialogService, ToastService } from '../shared/services';
 import { HeroesListComponent } from './heroes-list.component';
+
+const heroMock: HeroDetails = {
+  id: 1,
+  name: 'Test',
+};
 
 describe('HeroesListComponent', () => {
   let component: HeroesListComponent;
   let fixture: ComponentFixture<HeroesListComponent>;
+  let heroesApiService: jasmine.SpyObj<HeroesApiService>;
 
   beforeEach(async(() => {
+    heroesApiService = jasmine.createSpyObj<HeroesApiService>([
+      'getHeroList',
+      'deleteHero',
+    ]);
+
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [ HeroesListComponent ],
+      declarations: [HeroesListComponent],
       providers: [
         {
           provide: HeroesApiService,
-          useValue: {
-            getHeroList: () => of({ heroes: [ ]})
-          },
+          useValue: heroesApiService,
         },
         {
           provide: ToastService,
-          useValue: {},
+          useValue: {
+            error: () => {}
+          },
         },
         {
           provide: DialogService,
-          useValue: {},
+          useValue: {
+            confirm: () => of(true),
+          },
         },
       ],
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeroesListComponent);
     component = fixture.componentInstance;
+    heroesApiService.getHeroList.and.returnValue(of({ heroes: [heroMock] }));
     fixture.detectChanges();
   });
 
@@ -44,4 +57,15 @@ describe('HeroesListComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should have one hero', () => {
+    expect(component.dataSource.data).toBeTruthy();
+    expect(component.dataSource.data.length).toBe(1);
+  });
+
+  describe('delete hero', () => {
+    it('should call HeroesApiService.deleteHero ', () => {
+      component.onDeleteHero(heroMock);
+      expect(heroesApiService.deleteHero).toHaveBeenCalled();
+    });
+  });
 });
